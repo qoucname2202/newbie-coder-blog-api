de4efec7e3f99dd9050e62880539c5d42afb5f08
tay phan <tayphan13579@gmail.com>
2026-07-07 17:22:47 +0700

    git commit -m "Fix duplicate PasswordTooWeak: rename reset code to ResetPasswordWeak (00000206)"

--- NewbieCoder.Core/Constants/ResponseCodes.cs
+++ NewbieCoder.Core/Constants/ResponseCodes.cs
@@ -21,7 +21,9 @@ public static class ResponseCodes
     public const string InvalidEmailFormat = "00000201";
     public const string ResetTokenRequired = "00000201";
     public const string InvalidOrExpiredResetToken = "00000201";
-    public const string ResetPasswordWeak = "00000201";
+    public const string ResetPasswordWeak = "00000206";
+    public const string PasswordNotMatch = "00000207";
+    public const string PasswordReused = "00000208";
     public const string TooManyResetRequests = "00000429";
     public const string EmailSendFailed = "00000500";
     // Registration
@@ -63,6 +65,8 @@ public static class ResponseCodes
         EmailAlreadyExists => HttpStatusCodes.Conflict,
         UsernameAlreadyExists => HttpStatusCodes.Conflict,
         ResetPasswordWeak => HttpStatusCodes.BadRequest,
+        PasswordNotMatch => HttpStatusCodes.BadRequest,
+        PasswordReused => HttpStatusCodes.BadRequest,
         TermsNotAccepted => HttpStatusCodes.BadRequest,
         DeviceBlocked => HttpStatusCodes.Forbidden,
         DefaultRoleNotFound => HttpStatusCodes.InternalServerError,
